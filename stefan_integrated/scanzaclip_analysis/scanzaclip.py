256b2842e0c359a7c8ec592b555263175144472c
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-27 02:25:11 +0700

    Update scanzaclip.py

--- stefan_integrated/scanzaclip_analysis/scanzaclip.py
+++ stefan_integrated/scanzaclip_analysis/scanzaclip.py
@@ -1,1 +1,11 @@
+class ScanzaclipAnalyzer:
+    def __init__(self):
+        self.workflow = []
 
+    def analyze_clip(self, clip_data):
+        """
+        วิเคราะห์ workflow และโครงสร้างข้อมูล
+        """
+        steps = clip_data.get("steps", [])
+        self.workflow.append(steps)
+        return {"analysis_complete": True, "steps_count": len(steps)}
